5fd414143ae4c394b875dedf136e5ebb6a9e3bee
juliangiebel <juliangiebel@live.de>
2022-07-26 19:14:56 +0200

    Implement map selector

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## Requirements
 * Npm
 * Vite https://vitejs.dev/
-* OpenLayers
+* OpenLayers https://openlayers.org/
 
 ## Building
 
@@ -11,4 +11,71 @@
 yarn install
 npm run build
 ```
+
+## Map Json format
+
+Maps are provided as json files containing the image paths and settings for the different layers.
+The map selector is populated using the *list.json* file under *maps/list.json*
+The file structure needs to follow the following example:
+
+- maps
+    list.json
+    - {mapid}
+        - map.json
+        - {image}.png
+        - ...
+    - {mapid}
+     - ...
+
+### map.json:
+```json
+    {
+	"id": "{map id}", //(Must be the same as the folder name)
+	"name": "{map name}",
+	"tiled": false, //Whether the map uses tiles instead of one image (Not yet implemented!)
+	"url": "{map image url}", //The url to the map image used. Can be an external url
+	"extent": [x1, y1, x2, y2], //The size of the image in px
+    "attributions": "{Map author}",
+    "parallax": [
+		{
+			"scale": [0.1, 0.1],
+			"offset": [0, 0],
+			"simple": false, //If a parallax is marked as simple it doesn't tile and doesn't use layers
+			"minScale": 1, //This needs to be 1
+			"source": {
+				"url": "{parallax base image url}",
+				"extent": [x1, y1, x2, y2]
+			},
+			"layers": [
+				{
+					"url": "{parallax layer url}",
+					"composite": "{image composition}", //see https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/globalCompositeOperation
+					"parallaxScale": [0.1, 0.1]
+				},
+				...
+			]
+		},
+		{ //A simple parallax layer
+			"scale": [0.1, 0.1],
+			"offset": [800,450],
+			"simple": true,
+			"source": {
+				"url": "./image/misc/singularity.png",
+				"extent": [0, 0, 864, 864]
+			}
+		}
+	]
+}
+```
+
+### list.json
+```
+{
+	"maps": [
+		{"name": "{map name}", "id": "{map id}"},
+        ...
+	]
+}
+```
+
 
---
+++ js/MapSelector.js
@@ -0,0 +1,85 @@
+import Control from "ol/control/Control";
+import MapLoader from "./MapLoader";
+
+class MapSelector extends Control {
+
+	maps = [];
+	selected = {};
+	listElement = null;
+	selectedLabel = null;
+	mapChangeCallback = null;
+
+	static template = (selected) => `
+	<div id="map-selector" class="ol-unselectable ol-control">
+		<div class="selected-row">
+			<p id="map-selector-selected" class="selected-label">${selected.name}</p>
+			<button id="map-selector-button" >#</button>
+		</div>
+		<div id="map-selector-maps" class="hidden">
+		</div>
+	</div>
+	`;
+
+	constructor(opt_options) {
+		const options = opt_options || {};
+		const element = document.createRange().createContextualFragment(MapSelector.template(options.selected));
+
+		super({
+			element: element.getElementById('map-selector'),
+			target: options.target
+		});
+
+		this.maps = options.maps || [];
+		this.selected = options.selected;
+		this.mapChangeCallback = options.onMapChanged || function(){};
+
+		this.selectedLabel = element.getElementById('map-selector-selected');
+		this.listElement = element.getElementById('map-selector-maps');
+		element.getElementById('map-selector-button').addEventListener('click', () => this.listElement.classList.toggle('hidden'), false);
+
+		MapSelector.loadMapList().then(list => {
+			this.maps = list.maps;
+
+			this.updateList();
+	
+		});
+	}
+
+	handleMapChange(selected) {
+		const map = this.getMap();
+		MapLoader.loadLayers(map, selected.id);
+		
+		this.selected = selected;
+		this.updateList();
+
+		this.mapChangeCallback(selected);
+	}
+
+	updateList() {
+		this.listElement.innerHTML = '';
+		this.selectedLabel.innerHTML = this.selected.name;
+
+		for (const map of this.maps) {
+			if (this.selected.id === map.id)
+				continue;
+
+			const mapButton = document.createElement('button');
+			mapButton.innerHTML = map.name;
+			mapButton.addEventListener('click', this.handleMapChange.bind(this, map), false);
+
+			this.listElement.appendChild(mapButton);
+		}
+	}
+
+	static async loadMapList() {
+		const request = new Request(`maps/list.json`);
+		const response = await fetch(request);
+		if (!response.ok) {
+			throw new Error(`Failed to retrive map list! Status: ${response.status}`);
+		}
+		return await response.json();
+	}
+
+}
+
+export default MapSelector;
