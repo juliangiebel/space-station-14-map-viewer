1107d254137b7442a5a5c64db66a6e97a87325fd
juliangiebel <juliangiebel@live.de>
2023-06-07 13:08:12 +0200

    Replace toplevel await with .then

--- js/map.js
+++ js/map.js
@@ -4,32 +4,32 @@ import Markers from "./Markers";
 import Config from "./Config.js";
 
 //Load configuration
-await Config.loadConfiguration("config.json");
+Config.loadConfiguration("config.json").then(() => {
+	let map = null;
+	const query = new URLSearchParams(window.location.search);
+	const defaultMap = window.config.defaultMap;
 
-let map = null;
-const query = new URLSearchParams(window.location.search);
-const defaultMap = window.config.defaultMap;
+	const mapId = query.has('map') ? query.get('map').toLowerCase() : defaultMap;
+	const hideSelector = query.has('no-selector');
 
-const mapId = query.has('map') ? query.get('map').toLowerCase() : defaultMap;
-const hideSelector = query.has('no-selector');
+	function getMarkers()
+	{
+		return query.has('markers') ? Markers.parseMarkerList(query.get('markers')) : [];
+	}
 
-function getMarkers()
-{
-	return query.has('markers') ? Markers.parseMarkerList(query.get('markers')) : [];
-}
+	function onMapChangedHandler(selectedMap, map) {
+		const url = new URL(window.location);
+		url.searchParams.set('map',selectedMap.id);
+		window.history.pushState({}, '', url);
+		map.addLayer(Markers.drawMarkerLayer(getMarkers()));
+	}
 
-function onMapChangedHandler(selectedMap, map) {
-	const url = new URL(window.location);
-	url.searchParams.set('map',selectedMap.id);
-	window.history.pushState({}, '', url);
-	map.addLayer(Markers.drawMarkerLayer(getMarkers()));
-}
-
-MapLoader.loadMap(mapId).then((loadedMap) => {
-	map = loadedMap
-	
-	if (!hideSelector) map.addControl(new MapSelector({selected: {name: loadedMap.get('map-name'), id: mapId}, onMapChanged: onMapChangedHandler}));
-	
-	map.addLayer(Markers.drawMarkerLayer(getMarkers()));
-	window.olmap = map;
+	MapLoader.loadMap(mapId).then((loadedMap) => {
+		map = loadedMap
+		
+		if (!hideSelector) map.addControl(new MapSelector({selected: {name: loadedMap.get('map-name'), id: mapId}, onMapChanged: onMapChangedHandler}));
+		
+		map.addLayer(Markers.drawMarkerLayer(getMarkers()));
+		window.olmap = map;
+	});
 });
